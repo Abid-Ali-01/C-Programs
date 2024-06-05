a01227e5c1379b642b020c42f9fbe730713ab1b3
Abid Ali <117859453+Abid-Ali-01@users.noreply.github.com>
2024-06-05 16:23:41 +0500

    Update pointer_to_the_pointer.c

--- pointer_to_the_pointer.c
+++ pointer_to_the_pointer.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int value = 42;
-    int *ptr = &value;      // Pointer to an integer
-    int **ptr_to_ptr = &ptr; // Pointer to a pointer to an integer
+   int num = 20;
+  int  *ptr = &num; //pointer to the num
+   int **ptr2 = &ptr; // pointer to the pointer "ptr"
 
-    // Display the value and the addresses
-    printf("Value: %d\n", value);
-    printf("Address of value: %p\n", &value);
+   printf("The Value is %d \n", num); // prints value in "num"
+   printf("The address of value is %p\n", &num); // prints value of "num"
 
-    printf("Pointer ptr points to value: %d\n", *ptr);
-    printf("Address stored in ptr: %p\n", ptr);
-    printf("Address of ptr: %p\n", &ptr);
+   printf("The *ptr hold the value %d \n", *ptr); //Value stored in "ptr"
+   printf("Address of ptr is %p\n", *ptr); //Address of ptr
+   printf("Address stored in ptr is %p\n", ptr); //Address stored in "ptr" same as address of "num"
 
-    printf("Pointer to pointer ptr_to_ptr points to ptr which points to value: %d\n", **ptr_to_ptr);
-    printf("Address stored in ptr_to_ptr: %p\n", ptr_to_ptr);
-    printf("Address of ptr_to_ptr: %p\n", &ptr_to_ptr);
+   // Accessing value with the help of pointer to the pointer "**ptr2"
+   //It prints "num", because **ptr holds address of *ptr, and "*ptr" holds the address of "num"
+   printf("Pointer to the pointer **ptr2 points to the *ptr which holds the value %d\n", **ptr2);
 
+  //Prints address stored "*ptr", that is actually address of "num" in stored in "*ptr"
+   printf("Address stored in **ptr2  is %p\n", *ptr2);
+   
+   //Prints the address of "**ptr2"
+   printf("Address of ptr2 is %p\n", &ptr2);
     return 0;
 }
